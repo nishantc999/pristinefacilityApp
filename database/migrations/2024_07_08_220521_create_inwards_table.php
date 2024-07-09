1d214d1ab88087f9c2f38e9410abb5c1a0ce9c8b
nishantc999 <nishantc999@gmail.com>
2024-07-09 11:24:33 +0530

    d

---
+++ database/migrations/2024_07_08_220521_create_inwards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inwards', function (Blueprint $table) {
+            $table->id();
+            $table->string('purchase_no')->unique();
+            $table->string('vendor')->nullable();
+            $table->string('date')->nullable();
+            $table->string('pdf')->nullable();
+            $table->string('req')->nullable();
+            $table->text('sku_label_and_quantity')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inwards');
+    }
+};
